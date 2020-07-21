736631b8b8d8dec27e9de6f23888731f71d9482d
Marco Yuen <yuenmarco17@gmail.com>
2020-07-20 17:53:19 -0700

    Removed dummy variable in submitKeyword() and added further implementation of updateSiteResults(), both in script.js

--- static/script.js
+++ static/script.js
@@ -1,10 +1,10 @@
 async function submitKeyword() {
     event.preventDefault();
-    const sample = {keyword: "warm"};
-    const sampleJson = JSON.stringify(sample);
     const keyword = document.getElementById('kind').value;
+    const jsonToSend = {keyword: keyword};
+    const jsonStringToSend = JSON.stringify(jsonToSend);
 
-    const appResponse = await $.post("receiver", sampleJson, function(response) {
+    const appResponse = await $.post('receiver', jsonStringToSend, function(response) {
         return response;
     });
     appResponseArray = JSON.parse(appResponse);
@@ -14,13 +14,20 @@ async function submitKeyword() {
 function updateSiteResults(responseArray) {
     const sizeOfResponse = responseArray.length;
 
+    for (let i = 1; i <= 10; i++) {
+        resultNum = 'result' + i;
+        checkboxNum = resultNum + 'checkbox';
+        labelNum = resultNum + 'label';
 
-    for (let i = 1; i <= sizeOfResponse; i++) {
-        selection = 'result' + i + 'checkbox';
-        document.getElementById(selection).style.visibility = 'visible';
+        if (i <= sizeOfResponse) {
+            document.getElementById(checkboxNum).style.visibility = 'visible';
 
-        label = 'result' + i + 'label';
-        resultToAdd = responseArray[i-1];
-        document.getElementById(label).innerHTML = resultToAdd;
+            resultToAdd = responseArray[i-1];
+            document.getElementById(labelNum).innerHTML = resultToAdd;
+        } else {
+            document.getElementById(checkboxNum).style.visibility = 'hidden';
+            
+            document.getElementById(labelNum).innerHTML = '';
+        }
     }
 }
