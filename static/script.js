8bf07eb5bd364b88ebd344e9abb2a4e7be28985c
Marco Yuen <yuenmarco17@gmail.com>
2020-07-22 22:04:18 -0700

    Implemented returnRandomFromSelections() in script.py and added title to webpage

--- static/script.js
+++ static/script.js
@@ -1,3 +1,6 @@
+const updateSelectionErrorId = 'update_selection_error'
+let activeSelections = [];
+
 // Submits inputted keyword to app.py and returns response to webpage
 async function submitKeyword() {
     event.preventDefault();
@@ -54,25 +57,32 @@ function updateSelections() {
 }
 
 // Helper function for updateSelections()
-// Adds individual place names to "Selections" region of webpage or adds error message if no spaces left
+// Displays individual place names to "Selections" region of webpage or adds error message if no spaces left
 function addPlacesToSelections(places) {
     const firstOpenSpot = findFirstOpenSpot();
     if (firstOpenSpot > 0) {
         const numOpenSpots = 10 - firstOpenSpot + 1; //10 is number of total spots, +1 is to include first open spot
-        for (let i = 0; i < numOpenSpots; i++) {
+        for (let i = 0; i <= numOpenSpots; i++) {
 
             if (i >= places.length) {
+                break;
+            } else if (i === numOpenSpots) {
+                const displayErrorElement = document.getElementById(updateSelectionErrorId);
+                displayErrorElement.innerHTML = 'Some places were not added to the selections because there are now 10 selections. Please remove some before adding more.';
+                displayErrorElement.style.visibility = 'visible';
                 break;
             }
 
             const selectionNum = 'selection' + (i + firstOpenSpot);
             const selectionElement = document.getElementById(selectionNum);
 
-            selectionElement.innerHTML = places[i];
-            selectionElement.style.visibility = 'visible'
+            const place = places[i]
+            selectionElement.innerHTML = place;
+            selectionElement.style.visibility = 'visible';
+            activeSelections.push(place);
         }
     } else {
-        //TODO: print "no more spots" on page
+        document.getElementById(updateSelectionErrorId).innerHTML = 'There are already 10 selections. Please remove some before adding more.'
     }
 }
 
@@ -82,15 +92,38 @@ function findFirstOpenSpot() {
     for (let i = 1; i <= 10; i++) {
         const selection = 'selection' + i;
         const element = document.getElementById(selection).innerHTML;
-        if (!element || element === 'undefined') {
+        if (!element) {
             return i;
         }
     }
     return -1; // No open spot
 }
 
+// Chooses random place from activeSelections and displays it on the webpage
 function returnRandomFromSelections() {
-    //stub
+    const numCurrentSelections = activeSelections.length;
+    if (numCurrentSelections === 0) {
+        document.getElementById('place_result').innerHTML = "There aren't any places in the selections list!";
+    } else {
+        const randomNumber = getRandomNumber(numCurrentSelections);
+        document.getElementById('place_result').innerHTML = activeSelections[randomNumber];
+    }
+    makeResultVisible();    
+}
+
+// Helper function for returnRandomFromSelections()
+// Returns a random number from 0 to a given number, not including the given number
+function getRandomNumber(maximum) {
+    return Math.floor(Math.random() * maximum);
+}
+
+// Helper function for returnRandomFromSelections()
+// Changes visibility of result of returnRandomFromSelections() to be visible on the webpage
+function makeResultVisible() {
+    const elements = document.getElementsByClassName('returned_result');
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.visibility = 'visible';
+    }
 }
 
 function returnRandom() {
